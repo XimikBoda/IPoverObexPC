4a16dde5fbfedee27765f5bef5c733b3b1e727af
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-01-23 15:15:18 +0200

    Some rename 3

---
+++ bt/BTSock.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <queue>
+
+#ifdef WIN32
+#include <winrt/Windows.Devices.Bluetooth.h>
+#include <winrt/Windows.Networking.Sockets.h>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::Bluetooth;
+using namespace Windows::Devices::Bluetooth::Rfcomm;
+using namespace Windows::Networking::Sockets;
+#endif // WIN32
+
+class BTSock {
+#ifdef WIN32W
+	StreamSocket sock;
+
+#endif // WIN32
+public:
+};
+
+class BTSockListener {
+#ifdef WIN32
+	RfcommServiceId serviceId;
+	RfcommServiceProvider serviceProvider;
+	StreamSocketListener ssl;
+	std::queue<StreamSocket> socks_queue;
+
+	IAsyncAction OnConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args);
+#endif // WIN32
+public:
+	BTSockListener();
+	BTSockListener(uint16_t id);
+
+	void bind();
+
+	bool accept();
+};
+
---
+++ bt/BTSock_Windows.cpp
@@ -0,0 +1,58 @@
+#ifdef WIN32
+#include "bt.h"
+
+#include <iostream>
+#include <functional>
+
+#include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.ApplicationModel.Background.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Networking.Sockets.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Devices.Bluetooth.h>
+#include <winrt/Windows.Devices.Bluetooth.Rfcomm.h>
+#include <winrt/Windows.Devices.Bluetooth.Background.h>
+#include <winrt/Windows.Devices.Bluetooth.Advertisement.h>
+#include <winrt/Windows.Devices.Bluetooth.GenericAttributeProfile.h>
+#include <winrt/Windows.Devices.Enumeration.h>
+
+using namespace Windows::Networking::Sockets;
+
+
+BTSockListener::BTSockListener() :
+	serviceId(RfcommServiceId::ObexObjectPush()),
+	serviceProvider(RfcommServiceProvider::CreateAsync(serviceId).get())
+{
+
+}
+
+BTSockListener::BTSockListener(uint16_t id) :
+	serviceId(RfcommServiceId::FromShortId(id)),
+	serviceProvider(RfcommServiceProvider::CreateAsync(serviceId).get())
+{
+
+}
+
+IAsyncAction BTSockListener::OnConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args) {
+	StreamSocket sock = args.Socket();
+	socks_queue.push(sock);
+
+	std::wcout << "Conection: \n";
+	std::wcout << sock.Information().RemoteHostName().ToString().c_str() << '\n';
+
+	auto bdevice = BluetoothDevice::FromHostNameAsync(sock.Information().RemoteHostName()).get();
+	std::wcout << bdevice.Name().c_str() << '\n';
+	return 0;
+}
+
+void BTSockListener::bind() {
+	ssl.ConnectionReceived({ this, &BTSockListener::OnConnectionReceived });
+	ssl.BindServiceNameAsync(serviceId.AsString(), SocketProtectionLevel::BluetoothEncryptionAllowNullAuthentication).get();
+	
+	serviceProvider.StartAdvertising(ssl, true);
+}
+
+
+#endif // WIN32
+
