be344de8bb1cbdad05f9804031c84f79e47c6e95
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-01-26 00:00:33 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <thread>
 #include <BTSockListener.h>
 #include <OBEXServer.h>
@@ -11,16 +12,21 @@ void workerIn(BTSock btsocks) {
 	ReaderFromFunc reader([=, &btsocks](size_t len, bool partial){return btsocks.read(len); });
 	OBEXServer OBEXs;
 	OBEXs.reader = reader;
-	OBEXs.writer = [=, &btsocks](vec len) { btsocks.write(len); };
-	//OBEXs.run();
+	OBEXs.writer = [=, &btsocks](vec buf) { btsocks.write(buf); };
+	OBEXs.stream_writer = [=, &btsocks](vec buf) { std::cout << std::string(buf.begin(), buf.end()); };
+	OBEXs.run();
 }
 
 void workerOut(BTSock btsockc) {
 	OBEXClient OBEXc;
 	ReaderFromFunc reader([=, &btsockc](size_t len, bool partial) {return btsockc.read(len); });
 	OBEXc.reader = reader;
-	OBEXc.writer = [=, &btsockc](vec len) { btsockc.write(len); };
+	OBEXc.writer = [=, &btsockc](vec buf) { btsockc.write(buf); };
 	OBEXc.connet();
+	OBEXc.initPutStream("IpOverObex.txt", 0x7FFFFFFF);
+
+	std::string str = "Hi from pc\n";
+	OBEXc.PutStreamData(vec(str.begin(), str.end()));
 }
 
 std::vector<std::thread> threads;
